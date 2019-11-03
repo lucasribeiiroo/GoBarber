244dadc224b8ce72f12c013cc510adcfbfa27744
Lucas Ribeiro <lucasribeiro@MacBook-Pro-de-Lucas.local>
2019-11-03 17:41:24 -0300

    feat: Verificacao de datas de agendamento com date-fns

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -24,10 +25,28 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with a provider!' });
     }
 
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted!' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart
+      }
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Appointment date not available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.user_id,
       provider_id,
-      date
+      hourStart
     });
 
     return res.json(appointment);
