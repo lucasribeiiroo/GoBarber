1e79a4e17407c27384870f7973fe04348e02336c
Lucas Ribeiro <lucasribeiro@MacBook-Pro-de-Lucas.local>
2019-11-03 21:08:15 -0300

    feat: adicionado listagem de agenda de provider e instalacao do mongodb

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -26,4 +27,8 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
 // Agendar atendimento
 routes.post('/appointments', AppointmentController.store);
+// Listagem de Agendamentos
+routes.get('/appointments', AppointmentController.index);
+// Agenda do provider
+routes.get('/schedule', ScheduleController.index);
 export default routes;
